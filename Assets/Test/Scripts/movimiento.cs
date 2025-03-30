63bfb7d1b5aa792321200d83fc3f424c3b0d1cdb
Jesus Herrera <jesus.herreradelgadillo.30350@gmail.com>
2025-03-29 22:55:43 -0600

    [MODIFICO] el script de movimiento del jugador

--- Assets/Test/Scripts/movimiento.cs
+++ Assets/Test/Scripts/movimiento.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class movimiento : MonoBehaviour
 {
-    [SerializeField, Range(-100f, 100f)]
+    [SerializeField, Range(0f, 100f)]
     float velocidadMaxima = 10f;
 
+    [SerializeField, Range(0f, 100f)]
+    float aceleracionMaxima = 10f;
+
+    [SerializeField, Range(0f, 1f)]
+    float rebote = 0.5f;
+
     Vector3 velocidad;
+
+    [SerializeField]
+    Rect areaPermitida = new Rect(-5f, -5f, 10f, 10f);
+
     void Start()
     {
         
@@ -18,9 +29,17 @@ public class movimiento : MonoBehaviour
         jugadorInput.x = Input.GetAxis("Horizontal");
         jugadorInput.y = Input.GetAxis("Vertical");
         jugadorInput = Vector2.ClampMagnitude(jugadorInput, 1f);
-        Vector3 acceleracion = new Vector3(jugadorInput.x, 0f, jugadorInput.y) * velocidadMaxima;
-        velocidad += acceleracion * Time.deltaTime;
+
+        //Vector3 aceleracion = new Vector3(jugadorInput.x, 0f, jugadorInput.y) * velocidadMaxima;
+        Vector3 velocidadDeseada = new Vector3(jugadorInput.x, 0f, jugadorInput.y) * velocidadMaxima;
+        float cambioMaximoDeVelocidad = aceleracionMaxima * Time.deltaTime;
+
+        velocidad.x = Mathf.MoveTowards(velocidad.x, velocidadDeseada.x, cambioMaximoDeVelocidad);
+        velocidad.z = Mathf.MoveTowards(velocidad.z, velocidadDeseada.z, cambioMaximoDeVelocidad);
+
         Vector3 desplazamiento = velocidad * Time.deltaTime;
-        transform.localPosition += desplazamiento;
+        Vector3 nuevaPosicion = transform.localPosition + desplazamiento;
+       
+        transform.localPosition = nuevaPosicion;
     }
 }
