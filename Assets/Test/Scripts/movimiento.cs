0bba14dd77cf27d5d6950b9b03d69e65230e3fc4
Jesus Herrera <jesus.herreradelgadillo.30350@gmail.com>
2025-03-31 00:04:37 -0600

    [MODIFICO] el script de moviento de jugador a un cuerpo rigido

--- Assets/Test/Scripts/movimiento.cs
+++ Assets/Test/Scripts/movimiento.cs
@@ -9,17 +9,19 @@ public class movimiento : MonoBehaviour
     [SerializeField, Range(0f, 100f)]
     float aceleracionMaxima = 10f;
 
-    [SerializeField, Range(0f, 1f)]
-    float rebote = 0.5f;
+    //[SerializeField, Range(0f, 1f)]
+    //float rebote = 0.5f;
 
-    Vector3 velocidad;
+    Vector3 velocidad, velocidadDeseada;
 
-    [SerializeField]
-    Rect areaPermitida = new Rect(-5f, -5f, 10f, 10f);
+    Rigidbody cuerpoRigido;
 
-    void Start()
+    //[SerializeField]
+    //Rect areaPermitida = new Rect(-5f, -5f, 10f, 10f);
+
+    void Awake()
     {
-        
+        cuerpoRigido = GetComponent<Rigidbody>();
     }
 
 
@@ -30,16 +32,20 @@ public class movimiento : MonoBehaviour
         jugadorInput.y = Input.GetAxis("Vertical");
         jugadorInput = Vector2.ClampMagnitude(jugadorInput, 1f);
 
-        //Vector3 aceleracion = new Vector3(jugadorInput.x, 0f, jugadorInput.y) * velocidadMaxima;
-        Vector3 velocidadDeseada = new Vector3(jugadorInput.x, 0f, jugadorInput.y) * velocidadMaxima;
+        velocidadDeseada = new Vector3(jugadorInput.x, 0f, jugadorInput.y) * velocidadMaxima;
+        
+    }
+
+    //En FixedUpdate se llama al comiezo de cada paso de una simulacion de fisicas
+    void FixedUpdate()
+    {
+        velocidad = cuerpoRigido.linearVelocity;
         float cambioMaximoDeVelocidad = aceleracionMaxima * Time.deltaTime;
 
         velocidad.x = Mathf.MoveTowards(velocidad.x, velocidadDeseada.x, cambioMaximoDeVelocidad);
         velocidad.z = Mathf.MoveTowards(velocidad.z, velocidadDeseada.z, cambioMaximoDeVelocidad);
 
-        Vector3 desplazamiento = velocidad * Time.deltaTime;
-        Vector3 nuevaPosicion = transform.localPosition + desplazamiento;
-       
-        transform.localPosition = nuevaPosicion;
+        
+        cuerpoRigido.linearVelocity = velocidad;
     }
 }
