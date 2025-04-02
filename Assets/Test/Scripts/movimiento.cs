38fcf0ee27688887a974eb96a101866006d2424c
Jesus Herrera <jesus.herreradelgadillo.30350@gmail.com>
2025-04-01 22:30:47 -0600

    Se modifico el script de movimiento del jugador ahora salta

--- Assets/Test/Scripts/movimiento.cs
+++ Assets/Test/Scripts/movimiento.cs
@@ -9,6 +9,18 @@ public class movimiento : MonoBehaviour
     [SerializeField, Range(0f, 100f)]
     float aceleracionMaxima = 10f;
 
+    [SerializeField, Range(0f, 10f)]
+    float alturaDeSalto = 2f;
+
+    [SerializeField, Range(0, 5)]
+    int cantidadDeSaltosAereos;
+
+    int faseDeSalto;
+
+    bool saltoDeseado;
+
+    bool tocaPiso;
+
     //[SerializeField, Range(0f, 1f)]
     //float rebote = 0.5f;
 
@@ -16,17 +28,25 @@ public class movimiento : MonoBehaviour
 
     Rigidbody cuerpoRigido;
 
-    //[SerializeField]
-    //Rect areaPermitida = new Rect(-5f, -5f, 10f, 10f);
+    
+
+    /*private enum estadosJugador
+    {
+        idle,
+        moviendo,
+        salto
+    }*/
 
     void Awake()
     {
+        //estadosJugador estadoJugador = estadosJugador.idle;
         cuerpoRigido = GetComponent<Rigidbody>();
     }
 
 
     void Update()
     {
+        saltoDeseado |= Input.GetButtonDown("Jump");
         Vector2 jugadorInput;
         jugadorInput.x = Input.GetAxis("Horizontal");
         jugadorInput.y = Input.GetAxis("Vertical");
@@ -39,13 +59,61 @@ public class movimiento : MonoBehaviour
     //En FixedUpdate se llama al comiezo de cada paso de una simulacion de fisicas
     void FixedUpdate()
     {
-        velocidad = cuerpoRigido.linearVelocity;
+        actualizarEstado();
         float cambioMaximoDeVelocidad = aceleracionMaxima * Time.deltaTime;
 
         velocidad.x = Mathf.MoveTowards(velocidad.x, velocidadDeseada.x, cambioMaximoDeVelocidad);
         velocidad.z = Mathf.MoveTowards(velocidad.z, velocidadDeseada.z, cambioMaximoDeVelocidad);
 
+        if(saltoDeseado)
+        {
+            saltoDeseado = false;
+            saltar();
+        }
         
         cuerpoRigido.linearVelocity = velocidad;
+        tocaPiso = false;
+    }
+
+    void OnCollisionEnter(Collision colision)
+    {
+        EvaluarColision(colision);
+    }
+
+    private void OnCollisionStay(Collision colision)
+    {
+        EvaluarColision(colision);
+    }
+
+    void EvaluarColision(Collision colision)
+    {
+        for(int i = 0; i < colision.contactCount; i++)
+        {
+            Vector3 normal = colision.GetContact(i).normal;
+            tocaPiso |= normal.y > 0.9f;
+        }
+    }
+
+    void saltar()
+    {
+        if(tocaPiso || faseDeSalto < cantidadDeSaltosAereos)
+        {
+            faseDeSalto++;
+            float velocidadDeSalto = Mathf.Sqrt(-2f * Physics.gravity.y * alturaDeSalto);
+            if(velocidad.y > 0)
+            {
+                velocidadDeSalto = Mathf.Max(velocidadDeSalto - velocidad.y, 0f);
+            }
+            velocidad.y += velocidadDeSalto;
+        }
+    }
+
+    void actualizarEstado ()
+    {
+        velocidad = cuerpoRigido.linearVelocity;
+        if(tocaPiso)
+        {
+            faseDeSalto = 0;
+        }
     }
 }
